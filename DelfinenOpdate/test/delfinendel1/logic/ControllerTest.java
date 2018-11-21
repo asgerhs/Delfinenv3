1994e480c42b8e9ecd11b40b0ba6f1518a8b63e9
Fred <44893926+FrederikLL@users.noreply.github.com>
2018-11-21 18:07:30 +0100

    ved ikke hvad jeg siger ja til men ser fint ud

--- DelfinenOpdate/test/delfinendel1/logic/ControllerTest.java
+++ DelfinenOpdate/test/delfinendel1/logic/ControllerTest.java
@@ -8,6 +8,7 @@ package delfinendel1.logic;
 import DelfinenPart1.data.DBConnector;
 import DelfinenPart1.data.DataAccessorDB;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -26,8 +27,10 @@ import static org.junit.Assert.*;
 public class ControllerTest {
     private Controller c;
     private Member mm;
-    private DBConnector connector;
-    
+    //private DBConnector connector = null;
+    //private Connection connection;
+    //DataAccessorDB da = null;
+
     
     public ControllerTest() {
     }
@@ -58,25 +61,38 @@ public class ControllerTest {
         //ved ikke lige hvordan man tester void metoder. i bund og grund skal vi jo teste om metoden skriver til databsen
         
        try{ 
-            Connection connection = connector.getConnection();
+            //da = new DataAccessorDB(new DBConnector());
+            String result = "";
+            String query = "SELECT * FROM top5 WHERE discipline = 'test3'";
+            DBConnector cc = new DBConnector();
+            Connection connection = cc.getConnection();
+            //assertNotNull(connection);
             Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
             connection.setAutoCommit(false);
+            stmt.executeUpdate("INSERT INTO top5(discipline) VALUES ('test3')");
+            while(rs.next())   
+            {
+                result = rs.getString("discipline");
+            }
+            
             
             //DBConnector dbcon = new DBConnector();
             
             //String query = "Select * from member WHERE firstname = '"+mm.getfName()+"';";
-            stmt.executeUpdate("INSERT INTO top5(discipline) VALUES ('test1')");
+            //stmt.executeUpdate("INSERT INTO top5(discipline) VALUES ('test')");
             //stmt.executeQuery("Select * from member WHERE firstname = '"+mm.getfName()+"' AND '"+mm.getAge()+";");
-            stmt.executeQuery("SELECT * FROM top5");
+            //stmt.executeQuery("SELECT * FROM top5");
+            //stmt.executeQuery("SELECT * FROM top5");
+            Member m = new Member(mm.getfName(), mm.getLname(), mm.getAge(), mm.getTeam(), mm.getSex(), mm.getMembership(), mm.getActivePassive());
+            c.createMember(m);
+            assertEquals("test3", result);
+            connection.rollback();
+            connection.close();
+            //stmt.executeQuery("DELETE FROM top5 WHERE discipline = 'test1'; ");
             
-            //dbcon.("1", "one");
             
-            assertEquals("test1", stmt.executeQuery("SELECT * FROM top5"));
             
-            stmt.executeQuery("DELETE FROM top5 where discipline = 'test1");
-           // Member m = new Member(mm.getfName(), mm.getLname(), mm.getAge(), mm.getTeam(), mm.getSex(), mm.getMembership(), mm.getActivePassive());
-            
-            //c.createMember(m);
             //Member mem = new Member("", "", 0,"","","","");
             //Member expected = mem;
             //Member actual = m;
